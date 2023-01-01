d749e869fc62aaf36513cc1e333d87849ceeb3e9
PR-KILLER <106992407+PR-KILLER@users.noreply.github.com>
2023-01-01 13:40:55 -0300

    Novos arquivos de js

---
+++ cursojs/DOM.js
@@ -0,0 +1,9 @@
+//Selecionar apenas um elemento
+//const addUserText = document.getElementById("add-user");
+//const addUserText = document.querySelector('Badd-user');
+//const myForm = document.querySelector(".item");
+
+//Selecionar múltiplos elementos
+//const aliItems = document.querySelectorAll(".items .item");
+const aliItems = document.getElementsByTagName("li");
+console.log(aliItems)
